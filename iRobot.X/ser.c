4a6ce38b86508553028142034865229ab3a796c3
Brendan <starksta4@gmail.com>
2016-09-01 20:05:39 +1000

    Simplified drive, neatened up the code
    Made a drive() function that accepts arguements for speed and radius,
    moved it to ser.c to make the main.c a bit simpler.

--- iRobot.X/ser.c
+++ iRobot.X/ser.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include "ser.h"
+#include "lcd.h"
 
 void ser_init(void){
 //	PORTC = 0;
@@ -34,4 +35,31 @@ unsigned char ser_getch(){
 	while(!RCIF);
 	unsigned char rxbyte = RCREG;
 	return rxbyte;
+}
+
+//Drive command (Velocity high, Velocity low, Radius high, Radius low)
+//Radius of 0x7FFF is a straight line
+//Negative radius turns right
+void Drive(signed int speedH, signed int speedL, signed int radH, signed int radL){
+    ser_putch(137);
+        ser_putch(speedH);
+        ser_putch(speedL);
+        ser_putch(radH);
+        ser_putch(radL);
+}
+
+void getDistTrav(void){
+    ser_putch(142);     //Requests packet of sensor data
+    ser_putch(19);      //Specifies distance packet for request
+    
+    ser_getch();        //Gets the distance value returned as 2 bytes, high byte first.
+    highByte = rxbyte;  //Puts the high byte into variable
+    ser_getch();        //Gets the low byte of the sensor packet
+    lowByte = rxbyte;   //Puts the low byte into variable
+                
+    distTrav = (256*highByte + lowByte);    //Distance traveled since data was last requested
+    totalDistTrav = ((totalDistTrav + distTrav)/10);  //Total distance traveled since button push in CM
+                
+    lcdSetCursor(0b11000000);   //Second row, first position
+    lcdWriteToDigitBCD(totalDistTrav);  //Print the total distance to LCD
 }
